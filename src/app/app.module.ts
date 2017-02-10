944945de470323511a2988d174f49e6f87c28f7e
djomarko <djo.marko@gmail.com>
2017-02-10 18:44:42 +1300

    adding in posts/card module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CardModule } from './card/card.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardModule
   ],
   imports: [
     BrowserModule,
---
+++ src/app/card/card.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PostComponent } from './post/post.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [PostComponent]
+})
+export class CardModule { }
